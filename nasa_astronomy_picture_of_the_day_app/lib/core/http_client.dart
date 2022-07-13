1e429fbbabb25de28f0c4dfadf40300cf5081388
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-13 17:44:28 -0300

    added packages to pubspec and add entity add

---
+++ nasa_astronomy_picture_of_the_day_app/lib/core/http_client.dart
@@ -0,0 +1,4 @@
+class HttpClient {
+  static const String baseUrl = "https://api.nasa.gov/planetary/apod";
+  static const String apiKey = "uu8DEr43eF20KKXHvgVZQJeIwvJa5903Ny71jnOy";
+}
---
+++ nasa_astronomy_picture_of_the_day_app/lib/domain/entities/picture_of_the_day_entity.dart
@@ -0,0 +1,9 @@
+class PictureOfTheDayEntity {
+  final DateTime date;
+  final String explanation;
+  final String hdurl;
+  final String url;
+  final String title;
+
+  PictureOfTheDayEntity(this.date, this.explanation, this.hdurl, this.url, this.title);
+}
